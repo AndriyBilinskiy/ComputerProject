1cd865355e5b4fe9c37d7db3d1751e77c3ce9848
Andriy Bilinskiy <andriy@intteh.com>
2021-12-20 15:50:16 +0200

    fixed raise error bag

--- main.py
+++ main.py
@@ -8,15 +8,32 @@ def count(start: Union[int, float] = 0, step: Union[int, float] = 1):
     Returns an generator of the infinite sequence of nuberes spaced by step
     starting with start
     """
-    if (not isinstance(start, int) or not isinstance(step, int)
-        or not isinstance(start, float) or not isinstance(step, float)):
-        raise TypeError
     number = start
     while True:
         yield number
         number += step
 
+print(count(2,2))
+def combinations_with_replacement(iterable, r:int):
+    """
+    This function returns r length combinations with replacrment(Order doesn't metter)
+    """
+    iterable = tuple(iterable)
+    lenth = len(iterable)
+    if not lenth and r:
+        return
+    indices = [0] * r
+    yield tuple(iterable[i] for i in indices)
+    while True:
+        for i in reversed(range(r)):
+            if indices[i] != lenth - 1:
+                break
+        else:
+            return
+        indices[i:] = [indices[i] + 1] * (r - i)
+        yield tuple(iterable[i] for i in indices)
+    
+#(list (i for i in (combinations_with_replacement('ABC', 2))))
 
-def combinations_with_replacement(iterable, r):
-    pass
-
+for i in count(start=2, step=2):
+    print(i)
