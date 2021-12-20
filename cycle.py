4ad35fe2ff23b98b41c6f58683641eb9e3274b80
kirilltm04 <92577092+kirilltm04@users.noreply.github.com>
2021-12-20 17:53:23 +0200

    Update cycle.py

--- cycle.py
+++ cycle.py
@@ -1,14 +1,29 @@
-def cycle(iterable):
+def cycle(iterable, endpoint=None):
     """
     Function for itertools.cycle()
     Makes an iterable cycle of every element.
     Returns a generator of a cycle
-    :param iterable: optional
+    :param iterable: str
+    :param endpoint: optional
+    for _ in cycle(iterable): print("x")
     """
-    ans: list = []
-    for i in iterable:
-        yield i
-        ans.append(i)
-    while ans:
-        for i in ans:
-            yield i
+    try:
+        # processing exceptions
+        if endpoint is not None:
+            # making an endpoint
+            ans: list = []
+            # an empty list for an answer
+            for letters in iterable:
+                yield letters
+                # a generated object for iterable var
+                ans.append(letters)
+                # appending the generator in the empty list
+            while ans:
+                # excluding the None and negative cases
+                for letters in ans:
+                    yield letters
+                    # returning a generator of the ans list
+
+    except TypeError:
+        # preventing the Type error
+        print("Type error")
